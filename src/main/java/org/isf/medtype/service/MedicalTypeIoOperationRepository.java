bf6f09f2b32720efb81e478e99efcc954f3be72d
Mohit Rajeshkumar Sojitra <mh546676@dal.ca>
2024-11-25 09:40:53 -0400

    Test cased added

--- src/main/java/org/isf/medtype/service/MedicalTypeIoOperationRepository.java
+++ src/main/java/org/isf/medtype/service/MedicalTypeIoOperationRepository.java
@@ -25,9 +25,7 @@ import java.util.List;
 
 import org.isf.medtype.model.MedicalType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
